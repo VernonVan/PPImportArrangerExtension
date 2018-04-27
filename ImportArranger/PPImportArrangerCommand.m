e79a7aa4cd74c75cf0d6a28940472d127a03086d
范圣强 <fanshengqiang@myzaker.com>
2018-04-27 10:28:12 +0800

    - 将当前文件对应的头文件放到第一行

--- ImportArranger/PPImportArrangerCommand.m
+++ ImportArranger/PPImportArrangerCommand.m
@@ -8,10 +8,12 @@
 
 #import "PPImportArrangerCommand.h"
 
-#define ArrangeActiveFile    @"ArrangeActiveFile"
 #define ArrangeSelectedLines @"ArrangeSelectedLines"
 
-@implementation PPImportArrangerCommand
+@implementation PPImportArrangerCommand {
+    NSMutableArray<NSString *> *_classNames;
+    BOOL _isSelectedLines;
+}
 
 - (void)performCommandWithInvocation:(XCSourceEditorCommandInvocation *)invocation completionHandler:(void (^)(NSError *_Nullable nilOrError))completionHandler
 {
@@ -22,17 +24,20 @@
         NSRange selectedLineRange = NSMakeRange(textRange.start.line, textRange.end.line - textRange.start.line + 1);
         lines = [invocation.buffer.lines subarrayWithRange:selectedLineRange];
         firstLine = textRange.start.line;
+        _isSelectedLines = YES;
     } else {
         lines = invocation.buffer.lines;
+        _isSelectedLines = NO;
     }
-    
+
     if (!lines || !lines.count) {
         completionHandler(nil);
         return;
     }
 
     NSMutableArray<NSString *> *importLines = [[NSMutableArray alloc] init];
-    
+    _classNames = [[NSMutableArray alloc] init];
+
     for (NSUInteger index = 0, max = lines.count; index < max; index++) {
         NSString *line = lines[index];
         NSString *pureLine = [line stringByReplacingOccurrencesOfString:@" " withString:@""];       // 去掉多余的空格，以防被空格干扰没检测到 #import
@@ -44,6 +49,9 @@
                 firstLine = index;      // 记住第一行 #import 所在的行数，用来等下重新插入的位置
             }
         } else if ([pureLine hasPrefix:@"@implementation"]) {
+            NSString *className = [pureLine stringByReplacingOccurrencesOfString:@"@implementation" withString:@""];
+            className = [className stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            [_classNames addObject:className];
         }
     }
 
@@ -60,7 +68,7 @@
         // 重新插入排好序的 #import 行
         [invocation.buffer.lines insertObjects:sortedImportLines atIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(firstLine, sortedImportLines.count)]];
         // 选中所有 #import 行
-        [invocation.buffer.selections addObject:[[XCSourceTextRange alloc] initWithStart:XCSourceTextPositionMake(firstLine, 0) end:XCSourceTextPositionMake(firstLine + sortedImportLines.count, sortedImportLines.lastObject.length)]];
+        [invocation.buffer.selections addObject:[[XCSourceTextRange alloc] initWithStart:XCSourceTextPositionMake(firstLine, 0) end:XCSourceTextPositionMake(firstLine + sortedImportLines.count - 1, sortedImportLines.lastObject.length)]];
     }
 
     completionHandler(nil);
@@ -81,6 +89,23 @@
     if (systemImports.count) {
         [sortedImports removeObjectsInArray:systemImports];
         [sortedImports insertObjects:systemImports atIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(0, systemImports.count)]];
+    }
+    
+    // 把当前文件对应的头文件放在最前面
+    NSString *currentHeaderLine = nil;
+    if (!_isSelectedLines && _classNames.count) {
+        for (NSString *className in _classNames) {
+            for (NSString *line in sortedImports) {
+                if ([line containsString:className]) {
+                    currentHeaderLine = line;
+                    break;
+                }
+            }
+        }
+    }
+    if (currentHeaderLine) {
+        [sortedImports removeObject:currentHeaderLine];
+        [sortedImports insertObject:currentHeaderLine atIndex:0];
     }
 
     return sortedImports;
